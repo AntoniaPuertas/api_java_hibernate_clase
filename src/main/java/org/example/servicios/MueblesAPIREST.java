6dfb59a7f7cbc27f4f202888b434a9d2d88690b4
Totilla <tsm.antonia@gmail.com>
2025-01-31 09:05:36 +0100

    actualizando respuestas

--- src/main/java/org/example/servicios/MueblesAPIREST.java
+++ src/main/java/org/example/servicios/MueblesAPIREST.java
@@ -1,12 +1,14 @@
 package org.example.servicios;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.example.dao.MuebleDAOInterface;
 import org.example.entidades.Mueble;
 import spark.Spark;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MueblesAPIREST {
@@ -29,7 +31,8 @@ public class MueblesAPIREST {
         Spark.get("/muebles", (request, response) -> {
             List<Mueble> muebles = dao.devolverTodos();
             response.type("application/json");
-            return createJsonResponse("200", gson.toJson(muebles));
+            response.status(200);
+            return createJsonResponse("200", muebles);
         });
 
         //endpoint para obtener un mueble por su id
@@ -37,11 +40,14 @@ public class MueblesAPIREST {
             Long id = Long.parseLong(request.params(":id"));
             Mueble mueble = dao.buscarPorId(id);
             response.type("application/json");
+            List<Mueble> muebleList = new ArrayList<>();
             if(mueble != null){
-                return createJsonResponse("200", gson.toJson(mueble));
+                muebleList.add(mueble);
+                response.status(200);
+                return createJsonResponse("200", muebleList);
             }else{
                 response.status(404);
-                return createJsonResponse("404", "mueble no encontrado");
+                return createJsonResponse("Mueble no encontrado", muebleList);
             }
         });
 
@@ -54,7 +60,10 @@ public class MueblesAPIREST {
             Mueble creado = dao.create(nuevoMueble);
 
             response.type("application/json");
-            return createJsonResponse("200", gson.toJson(creado));
+            List<Mueble> muebleList = new ArrayList<>();
+            muebleList.add(creado);
+            response.status(200);
+            return createJsonResponse("200", muebleList);
         });
 
         Spark.put("muebles/id/:id", (request, response) -> {
@@ -63,13 +72,17 @@ public class MueblesAPIREST {
             String body = request.body();
             Mueble muebleActualizado = gson.fromJson(body, Mueble.class);
             muebleActualizado.setId(id);
-
+            List<Mueble> muebleList = new ArrayList<>();
             Mueble actualizado = dao.actualizar(muebleActualizado);
+            response.type("application/json");
             if(actualizado != null){
-                return createJsonResponse("200", gson.toJson(actualizado));
+
+                muebleList.add(actualizado);
+                response.status(200);
+                return createJsonResponse("200", muebleList);
             }else{
                 response.status(404);
-                return createJsonResponse("404", "No se ha podido realizar la actualización");
+                return createJsonResponse("No se ha podido realizar la actualización", muebleList);
             }
         });
 
@@ -77,21 +90,36 @@ public class MueblesAPIREST {
             Long id = Long.parseLong(request.params(":id"));
             boolean eliminado = dao.deleteById(id);
             response.type("application/json");
+            List<Mueble> muebleList = new ArrayList<>();
             if(eliminado){
-                return createJsonResponse("200", "Mueble eliminado correctamente");
+                response.status(200);
+                return createJsonResponse("Mueble eliminado correctamente", muebleList);
             }else{
                 response.status(404);
-                return createJsonResponse("404", "No se pudo realizar la eliminación");
+                return createJsonResponse("No se puedo realizar la eliminación", muebleList);
             }
         });
     }
 
-    public String createJsonResponse(String status, String message) {
+//    public String createJsonResponse(String status, String message) {
+//        Gson gson = new Gson();
+//
+//        JsonObject jsonResponse = new JsonObject();
+//        jsonResponse.addProperty("status", status);
+//        jsonResponse.addProperty("message", message);
+//        return gson.toJson(jsonResponse);
+//    }
+
+    public String createJsonResponse(String status, List<Mueble> messages) {
         Gson gson = new Gson();
 
         JsonObject jsonResponse = new JsonObject();
         jsonResponse.addProperty("status", status);
-        jsonResponse.addProperty("message", message);
+
+        // Convertir la lista de objetos a JsonArray
+        JsonArray messageArray = gson.toJsonTree(messages).getAsJsonArray();
+        jsonResponse.add("message", messageArray);
+
         return gson.toJson(jsonResponse);
     }
 }
